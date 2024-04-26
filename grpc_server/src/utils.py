e05b9fe0f64b5d179dd785123a9767a9a636d8fa
Shakib Hossain <3992124+shakib609@users.noreply.github.com>
2024-04-27 02:51:18 +0600

    Update convert string to timestamp utility function

--- grpc_server/src/utils.py
+++ grpc_server/src/utils.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 def convert_string_to_timestamp(timestamp_str: str) -> Timestamp:
+    time = Timestamp()
     formatted_time_str = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
-    timestamp = formatted_time_str.timestamp()
-    return Timestamp(seconds=timestamp)
+    time.FromDatetime(formatted_time_str)
+    return time
